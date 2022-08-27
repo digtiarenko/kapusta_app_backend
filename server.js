676b9b8814952acd455346ecc88d230447636bdb
Valentyn Digtiarenko <baggins999@gmail.com>
2022-08-27 13:18:10 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(
+        'Database connection successful \nServer running. Use our API on port:',
+        PORT,
+      );
+    }),
+  )
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
