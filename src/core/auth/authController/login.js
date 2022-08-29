3d978cf3a78e239506dd09796d71f84dcc7734d6
Tetiana Legois <mme.tanyalegois@gmail.com>
2022-08-29 21:26:51 +0100

    auth

--- src/core/auth/authController/login.js
+++ src/core/auth/authController/login.js
@@ -1,5 +1,37 @@
-const login = (req, res) => {
-  res.send('login controller');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const userService = require('../../user/userService');
+const { createError } = require('../../../helpers');
+const { JWT_ACCESS_SECRET, JWT_ACCESS_EXPIRE_TIME } = process.env;
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userService.getUserByEmail(email);
+  if (!user) {
+    throw createError(409, `Email ${email} not found`);
+  }
+  const passCompare = bcrypt.compareSync(password, user.password);
+  if (!passCompare) {
+    throw createError(409, 'password is wrong');
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, JWT_ACCESS_SECRET, {
+    expiresIn: JWT_ACCESS_EXPIRE_TIME,
+  });
+
+  await userService.updateUserById(user._id, token);
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      token,
+    },
+  });
 };
 
 module.exports = login;
--- src/core/auth/authController/register.js
+++ src/core/auth/authController/register.js
@@ -12,14 +12,16 @@ const register = async (req, res) => {
     password,
     Number(process.env.HASH_POWER),
   );
-
+  const name = email.slice(0, email.indexOf('@'));
   const newUser = await userService.addUser({
     ...req.body,
     password: hashPassword,
+    name,
   });
 
   res.status(201).json({
     user: {
+      name: newUser.name,
       email: newUser.email,
       balance: newUser.balance,
     },
