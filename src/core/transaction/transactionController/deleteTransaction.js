80f52230afc972fc29d95e405cf8e56c4f30c5fa
Marharyta Bosak <aboimova.rita@gmail.com>
2022-08-30 12:00:02 +0300

    Update deleteTransaction.js

--- src/core/transaction/transactionController/deleteTransaction.js
+++ src/core/transaction/transactionController/deleteTransaction.js
@@ -1,5 +1,17 @@
-const deleteTransaction = (req, res) => {
-  res.send('deleteTransaction controller');
+const {createError} = require('../../../helpers');
+
+const {Transaction} = require('../transactionModel');
+
+const deleteTransaction = async(req, res) => {
+  const { transactionId } = req.params;
+  const result = await Transaction.findByIdAndRemove(transactionId);
+  if (!result) {
+    throw createError(404);
+  }
+  res.status(200).json({
+    message: 'transaction deleted',
+    data: { result },
+  });
 };
 
 module.exports = deleteTransaction;
