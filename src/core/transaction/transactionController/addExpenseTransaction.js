0505a1cd5d93dbbf0d6bb6571ee84803423a9fdf
Valentyn Digtiarenko <baggins999@gmail.com>
2022-08-30 20:00:47 +0300

    feature/ added expense transaction ctrl

--- src/core/transaction/transactionController/addExpenseTransaction.js
+++ src/core/transaction/transactionController/addExpenseTransaction.js
@@ -1,5 +1,17 @@
-const addExpenseTransaction = (req, res) => {
-  res.send('addExpenseTransaction controller');
+const transactionService = require('../transactionService');
+
+const addExpenseTransaction = async (req, res) => {
+  const { _id } = req.user;
+  console.log('id', _id);
+
+  const result = await transactionService.createExpense({
+    ...req.body,
+    owner: _id,
+    type: 'expenses',
+  });
+  res
+    .status(201)
+    .json({ message: 'Expenses added successfully', data: { result } });
 };
 
 module.exports = addExpenseTransaction;
