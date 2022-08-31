538b21b89ee86070cd0845198a725ec831a3da96
Marharyta Bosak <aboimova.rita@gmail.com>
2022-08-31 18:41:47 +0300

    add dal findTransactions

--- src/core/transaction/transactionController/getTransactions.js
+++ src/core/transaction/transactionController/getTransactions.js
@@ -1,13 +1,15 @@
 const { createError } = require('../../../helpers');
 
-const { Transaction } = require('../transactionModel');
+const transactionService = require('../transactionService');
 
 const getTransactions = async (req, res) => {
   const { type } = req.params;
-  const result = await Transaction.find({ type });
-  if (!result) {
+
+  const result = await transactionService.findTransactions({ type });
+  if (result.length === 0) {
     throw createError(404);
   }
+
   res.status(200).json({ data: { result } });
 };
 
