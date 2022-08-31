9c16cb70f35fa018732de483b6ff2dded5f2915a
Marharyta Bosak <aboimova.rita@gmail.com>
2022-08-31 18:33:05 +0300

    Update getTransactions.js

--- src/core/transaction/transactionController/getTransactions.js
+++ src/core/transaction/transactionController/getTransactions.js
@@ -1,5 +1,14 @@
-const getExpenseTransactions = (req, res) => {
-  res.send('getExpenseTransactions controller');
+const { createError } = require('../../../helpers');
+
+const { Transaction } = require('../transactionModel');
+
+const getTransactions = async (req, res) => {
+  const { type } = req.params;
+  const result = await Transaction.find({ type });
+  if (!result) {
+    throw createError(404);
+  }
+  res.status(200).json({ data: { result } });
 };
 
-module.exports = getExpenseTransactions;
+module.exports = getTransactions;
