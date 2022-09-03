f2fa8af7f0e021201332949a533cb5bf7337c654
Tania Voronchuk <tanya.perfekt.pr@gmail.com>
2022-09-03 11:36:54 +0300

    fix

--- src/core/report/reportController/getIncomeReportByMonth.js
+++ src/core/report/reportController/getIncomeReportByMonth.js
@@ -1,30 +1,9 @@
-const { Transaction } = require('../../transaction/transactionModel');
+const transactionService = require('../../transaction/transactionService');
 
 const getIncomeReportByMonth = async (req, res) => {
-  const { _id, ObjectId } = req.user;
-  const result = await Transaction.aggregate([
-    {
-      $match: {
-        owner: ObjectId(_id),
-        type: 'income',
-      },
-    },
-    {
-      $group: {
-        _id: { month: { $month: '$createdAt' } },
-        totalSum: { $sum: '$value' },
-      },
-    },
-    { $limit: 6 },
-    {
-      $project: {
-        _id: 0,
-        month: '$_id.month',
-        totalSum: 1,
-      },
-    },
-  ]);
-  res.json(result);
+  const { _id } = req.user;
+  const result = await transactionService.getReportTransactionIncome({ _id });
+  return result;
 };
 
 module.exports = getIncomeReportByMonth;
