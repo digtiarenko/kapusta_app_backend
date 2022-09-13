0018137d2652ffd17ae732a05f1ca21b27ea6b82
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-09-13 19:23:04 +0300

    add verification category type

--- src/core/transaction/transactionController/addTransaction.js
+++ src/core/transaction/transactionController/addTransaction.js
@@ -1,11 +1,16 @@
+const { createError } = require('../../../helpers');
 const transactionService = require('../transactionService');
 
 const addExpenseTransaction = async (req, res) => {
   const { _id } = req.user;
+
   const transaction = await transactionService.createTransaction({
     ...req.body,
     owner: _id,
   });
+  if (!transaction) {
+    throw createError(400, 'Wrong category ID');
+  }
   res.status(201).json({
     message: 'Transaction created successfully',
     transaction,
--- src/core/transaction/transactionService/createTransaction.js
+++ src/core/transaction/transactionService/createTransaction.js
@@ -1,6 +1,12 @@
 const { Transaction } = require('../transactionModel');
+const { getCategoryById } = require('../../category/categoryService');
 
 const createTransaction = async transactionData => {
+  const { category: categoryId, type } = transactionData;
+  const category = await getCategoryById(categoryId);
+  if (!category || category.type !== type) {
+    return null;
+  }
   const result = await (
     await Transaction.create(transactionData)
   ).populate('category', 'name default');
